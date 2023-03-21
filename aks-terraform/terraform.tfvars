0d4512cc9b0a9183623b58554a55aa352f863814
Atisha Shaurya <94087803+Atisha-Shaurya@users.noreply.github.com>
2023-03-21 15:24:30 +0530

    Add files via upload

---
+++ aks-terraform/terraform.tfvars
@@ -0,0 +1,6 @@
+resource_group_name = "demo_rg"
+location = "westus"
+cluster_name = "aks-cluster"
+kubernetes_version = "1.26.0"
+node_count = 2
+agent_count = 2
---
+++ aks-terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  type = string
+}
+variable "location" {
+  type = string
+}
+variable "cluster_name" {
+  type = string
+}
+variable "kubernetes_version" {
+  type = string
+}
+variable "node_count" {
+  type = number
+}
